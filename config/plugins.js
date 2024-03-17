658e524b9eeb3e67d5fa59d9330833e8092a99d5
alief abdillah <aliefmabdillah09@gmail.com>
2024-03-17 20:35:46 +0700

    fix: modified provider for upload image to strapi

--- config/plugins.js
+++ config/plugins.js
@@ -38,12 +38,29 @@ module.exports = ({ env }) => ({
       },
     }
   },
-  upload: {
+  /* upload: {
     config: {
       providerOptions: {
         localServer: {
           maxage: 300000
         },
+      },
+    },
+  }, */
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_API_KEY'),
+        api_secret: env('CLOUDINARY_API_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {
+          folder: env("CLOUDINARY_FOLDER"),
+       },
+        delete: {},
       },
     },
   },
